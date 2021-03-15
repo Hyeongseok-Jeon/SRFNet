8708b2b479be46a4676190434816cfd2aa71868c
HyeongseokJeon 5900x <521hsgugu@kaist.ac.kr>
2021-03-15 12:47:24 +0900

    asdf

--- SRFNet/config.py
+++ SRFNet/config.py
@@ -35,8 +35,8 @@ def get_config(root_path, args):
         config["batch_size"] = 4
         config["val_batch_size"] = 4
     else:
-        config["batch_size"] = 16
-        config["val_batch_size"] = 16
+        config["batch_size"] = 12
+        config["val_batch_size"] = 12
 
     if args.pre == True:
         config["batch_size"] = 1
