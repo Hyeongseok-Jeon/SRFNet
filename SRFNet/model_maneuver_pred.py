de8a0f3398dc1ed259ff5cdac98b728b26f376d4
HyeongseokJeon 5900x <521hsgugu@kaist.ac.kr>
2021-03-28 05:25:09 +0900

    asdf

--- SRFNet/model_maneuver_pred.py
+++ SRFNet/model_maneuver_pred.py
@@ -48,7 +48,7 @@ if not os.path.isabs(config["save_dir"]):
 
 config["batch_size"] = 32
 config["val_batch_size"] = 32
-config["workers"] = 32
+config["workers"] = 0
 config["val_workers"] = config["workers"]
 
 """Dataset"""
