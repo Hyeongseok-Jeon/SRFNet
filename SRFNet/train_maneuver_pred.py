c6d8093b238024ee6c11aa0f59169294ca333c41
HyeongseokJeon 5900x <521hsgugu@kaist.ac.kr>
2021-03-28 05:41:33 +0900

    sadf

--- SRFNet/train_maneuver_pred.py
+++ SRFNet/train_maneuver_pred.py
@@ -97,7 +97,7 @@ def main():
             shutil.copy(os.path.join(src_dir, f), os.path.join(dst_dir, f))
 
     # Data loader for training
-    dataset = Dataset(config["train_split"], config, train=False)
+    dataset = Dataset(config["train_split"], config, train=True)
     train_loader = DataLoader(
         dataset,
         batch_size=config["batch_size"],
