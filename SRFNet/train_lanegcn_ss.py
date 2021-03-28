016671183183280c1bdd3b9f8aac5ed726278566
HyeongseokJeon 5900x <521hsgugu@kaist.ac.kr>
2021-03-28 15:34:27 +0900

    asdf

--- SRFNet/train_lanegcn_ss.py
+++ SRFNet/train_lanegcn_ss.py
@@ -211,6 +211,7 @@ def train(epoch, config, train_loader, net, loss, post_process, opt, val_loader=
         outputs.append(output0[0])
         loss_out0 = loss[0](outputs[0], data_copy[0])
         if opt[0] != None:
+            opt[1].zero_grad()
             opt[0].zero_grad()
             loss_out0["loss"].backward()
             lr0 = opt[0].step(epoch)
@@ -223,6 +224,7 @@ def train(epoch, config, train_loader, net, loss, post_process, opt, val_loader=
             outputs.append(output1[1])
             loss_out1 = loss[1](outputs[1], data_copy[1], losses[0])
             opt[1].zero_grad()
+            opt[0].zero_grad()
             loss_out1["loss"].backward()
             lr1 = opt[1].step(epoch)
             losses.append(loss_out1)
