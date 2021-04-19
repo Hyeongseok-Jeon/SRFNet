79914f09a6646c3538e913feb087d87b9a7aab8c
Hyeongseok-Jeon <66467102+Hyeongseok-Jeon@users.noreply.github.com>
2021-04-19 10:02:00 +0900

    Update train_ego_wrapper_supervised.py

--- SRFNet_new/train_ego_wrapper_supervised.py
+++ SRFNet_new/train_ego_wrapper_supervised.py
@@ -61,4 +61,108 @@ pretrained_dict = pre_trained_weight['state_dict']
 base_net.load_state_dict(pretrained_dict)
 base_net = base_net.cuda(config['gpu_id'])
 
+net = model.model_class(config, args, base_net)
+model = net.cuda(config['gpu_id'])
+
+dataset = SRF_data_loader(config, train=True)
+train_loader = DataLoader(
+    dataset,
+    batch_size=config["batch_size"],
+    shuffle=True,
+    num_workers=config["workers"],
+    collate_fn=collate_fn,
+    drop_last=True,
+)
+
+# Data loader for evaluation
+dataset = SRF_data_loader(config, train=False)
+val_loader = DataLoader(
+    dataset,
+    batch_size=config["val_batch_size"],
+    num_workers=config["val_workers"],
+    shuffle=True,
+    collate_fn=collate_fn,
+)
+l1loss = nn.SmoothL1Loss()
+loss_logging = Loss(config)
+post_process = PostProcess(config)
+os.makedirs(config['save_dir'], exist_ok=True)
+
+log = os.path.join(config['save_dir'], "log")
+
+if not os.path.exists(config['save_dir']):
+    os.makedirs(config['save_dir'])
+sys.stdout = Logger(log)
+
+src_dirs = [root_path]
+dst_dirs = [os.path.join(config['save_dir'], "files")]
+for src_dir, dst_dir in zip(src_dirs, dst_dirs):
+    files = [f for f in os.listdir(src_dir) if f.endswith(".py")]
+    if not os.path.exists(dst_dir):
+        os.makedirs(dst_dir)
+    for f in files:
+        shutil.copy(os.path.join(src_dir, f), os.path.join(dst_dir, f))
+
+start_time = time.time()
+for epoch in range(config["num_epochs"]):
+    metrics = dict()
+    for i, data in tqdm(enumerate(train_loader)):
+        with torch.no_grad():
+            actors, actors_idcs = base_net(data)
+        outputs = model(data[0], data[1], actors, actors_idcs)
+        batch_num = data[0].shape[0]
+        vehicle_per_batch = data[0][:, 11, 0, 0, 0, 0]
+        gt_preds = [data[0][i, 1, :int(vehicle_per_batch[i]), :30, :2, 0] for i in range(batch_num)]
+        gt = torch.cat([torch.repeat_interleave(gt_preds[i][1:2, :, :], 6, dim=0).unsqueeze(dim=0) for i in range(len(gt_preds))], dim=0)
+        loss = l1loss(outputs[:,1,:,:,:].cpu(), gt)
+
+        # actors should be tensor
+        output_reform = dict()
+        cls = [outputs[i:i+1, 0, :, 0, 0] for i in range(outputs.shape[0])]
+        reg = [outputs[i:i+1, 1, :, :, :] for i in range(outputs.shape[0])]
+        output_reform['cls'] = cls
+        output_reform['reg'] = reg
+        output_reform = [output_reform]
+
+        if i == 0 and epoch == 0:
+            loss_out = loss_logging(output_reform[0], data)
+            post_out = post_process(output_reform[0], data)
+            post_process.append(metrics, loss_out, post_out)
+            dt = time.time() - start_time
+            post_process.display(metrics, dt, epoch, 0.001)
+
+        opt.zero_grad()
+        loss.backward()
+        opt.step(epoch)
+
+        loss_out = loss_logging(output_reform[0], data)
+        post_out = post_process(output_reform[0], data)
+        post_process.append(metrics, loss_out, post_out)
+
+    if epoch % 2 == 1:
+        metrics = dict()
+        for i, data in tqdm(enumerate(val_loader)):
+            with torch.no_grad():
+                actors, actors_idcs = base_net(data)
+                outputs = model(data[0], data[1], actors, actors_idcs)
+                output_reform = dict()
+                cls = [outputs[i:i+1, 0, :, 0, 0] for i in range(outputs.shape[0])]
+                reg = [outputs[i:i+1, 1, :, :, :] for i in range(outputs.shape[0])]
+                output_reform['cls'] = cls
+                output_reform['reg'] = reg
+                output_reform = [output_reform]
+                
+                loss_out = loss_logging(output_reform[0], data)
+                post_out = post_process(output_reform[0], data)
+                post_process.append(metrics, loss_out, post_out)
+        dt = time.time() - start_time
+        post_process.display(metrics, dt, epoch, 0.001)
+        start_time = time.time()
+
+    save_ckpt(net, opt, config['save_dir'], epoch)
+    dt = time.time() - start_time
+    post_process.display(metrics, dt, epoch, 0.001)
+    start_time = time.time()
+    metrics = dict()
+    
 time.sleep(10)
